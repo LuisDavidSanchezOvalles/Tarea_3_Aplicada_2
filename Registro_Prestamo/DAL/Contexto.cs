56130debf8c169bd6c2cc1446d781031e27a2cbd
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-05-29 13:42:08 -0400

    Add project files.

---
+++ Registro_Prestamo/DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Registro_Prestamo.Models;
+
+namespace Registro_Prestamo.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+        public DbSet<Prestamos> Prestamos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = C:\Users\luisd\OneDrive\Escritorio\Universidad\Programacion Aplicada 2\Base_de_Datos_de_Blazor\PrestamosDB\Prestamos.db");
+        }
+    }
+}
---
+++ Registro_Prestamo/Models/Prestamos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Registro_Prestamo.Validations;
+
+namespace Registro_Prestamo.Models
+{
+    public class Prestamos
+    {
+        [Key]
+        [Required(ErrorMessage = "No debe de estar Vacío el campo 'PrestamoId'")]
+        [IdValidacion]
+        public int PrestamoId { get; set; }
+
+        [Required(ErrorMessage = "No debe de estar Vacío el campo 'Fecha'")]
+        [FechaNacimientoValidacion]
+        public DateTime Fecha { get; set; }
+        
+        [Required(ErrorMessage = "No debe de estar Vacío el campo 'PersonaId'")]
+        [IdValidacion]
+        public int PersonaId { get; set; }
+
+        [Required(ErrorMessage = "No debe de estar Vacío el campo 'Concepto'")]
+        public string Concepto { get; set; }
+
+        [Required(ErrorMessage = "No debe de estar Vacío el campo 'Monto'")]
+        [MontoValidacion]
+        public decimal Monto { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public Prestamos()
+        {
+            PrestamoId = 0;
+            Fecha = DateTime.Now;
+            PersonaId = 0;
+            Concepto = string.Empty;
+            Monto = 0;
+            Balance = 0;
+        }
+
+        public Prestamos(int prestamoId, DateTime fecha, int personaId, string concepto, decimal monto, decimal balance)
+        {
+            PrestamoId = prestamoId;
+            Fecha = fecha;
+            PersonaId = personaId;
+            Concepto = concepto;
+            Monto = monto;
+            Balance = balance;
+        }
+    }
+}
