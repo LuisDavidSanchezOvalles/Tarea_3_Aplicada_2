6f8e54cca4aeb5b797bd422cafd1edee5c4027ba
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-07-01 20:56:10 -0400

    Agregando Api a Prestamo Para La Tarea 7

---
+++ WebApplication1/Controllers/PrestamoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Registro_Prestamo.BLL;
+using Registro_Prestamo.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrestamoController : ControllerBase
+    {
+        // GET: api/Prestamo
+        [HttpGet]
+        public ActionResult<List<Prestamos>> Get()
+        {
+            return PrestamosBLL.GetPrestamos();
+        }
+
+        // GET: api/Prestamo/1
+        [HttpGet("{id}")]
+        public ActionResult<Prestamos> Get(int id)
+        {
+            return PrestamosBLL.Buscar(id);
+        }
+    }
+}
